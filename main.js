9ba5ffb897c104c95c0a110d92c039cbf0098f93
Rafi Muhammad <rafi.muhammad@mekari.com>
2022-10-19 14:42:23 +0700

    Change PORT

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var express = require("express")
 
+var PORT = process.env.PORT || 8002
 var app = express();
 var docs_handler = express.static(__dirname + '/docs/');
 app.use(docs_handler);
 //  start the server
-app.listen(8002);
+app.listen(PORT);
